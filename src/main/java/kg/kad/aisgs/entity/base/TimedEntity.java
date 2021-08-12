bb8d799c4a4b05654068107be811e19499af2ab8
kad <kmavlianov@it.gov.kg>
2021-08-12 21:58:47 +0600

    base timed entity

---
+++ src/main/java/kg/kad/aisgs/entity/base/TimedEntity.java
@@ -0,0 +1,41 @@
+package kg.kad.aisgs.entity.base;
+
+import kg.kad.aisgs.util.TimeHelper;
+import lombok.Getter;
+import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * created by kadyrbek.mavlyanov@gamil.com
+ * 12/8/21
+ */
+@Getter
+@Audited
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimedEntity extends BaseEntity {
+
+    private static final long serialVersionUID = -850788074338551061L;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public String getCreatedAtFormatted() {
+        return createdAt.format(TimeHelper.DATE_TIME_FORMATTER);
+    }
+
+    public String getUpdatedAtFormatted() {
+        return Objects.nonNull(updatedAt) ? updatedAt.format(TimeHelper.DATE_TIME_FORMATTER) : "";
+    }
+
+}
